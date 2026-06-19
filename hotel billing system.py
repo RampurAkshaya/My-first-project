eee2c80b32c4c011ff7d06de4095d4e7cc3ad79c
RampurAkshaya <goudakshaya217@gmail.com>
2026-06-19 12:13:35 +0530

    Update hotel billing system.py

--- hotel billing system.py
+++ hotel billing system.py
@@ -12,17 +12,13 @@ def apply_discount(total_amount):
 def display_bill(guest_name, final_amount):
     print("Hotel Name :", hotel_name)
     print("Guest Name :", guest_name)
-    print("Final Bill :", final_amount)
-
-# Input (NO prompt text)
+    print("Final Bill :", final_amount
 guest_name = input()
 room_charge = float(input())
 food_charge = float(input())
 service_charge = float(input())
 
-# Process
 total = calculate_bill(room_charge, food_charge, service_charge)
 final_amount = apply_discount(total)
 
-# Output
 display_bill(guest_name, final_amount)
